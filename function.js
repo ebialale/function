628bf6717862470cdaf674bae3f2e265c2c6317b
ebialale <63908688+ebialale@users.noreply.github.com>
2020-04-19 12:20:09 +0100

    odd and even

---
+++ function.js
@@ -0,0 +1,15 @@
+
+ 	function numTest(number) {
+	if (number%2 === 0)
+		return "bar";
+	else
+		return "foo";
+
+}
+
+
+console.log(numTest(9));
+
+/**
+ Still tring to figure out how that of prime number works.
+ */
