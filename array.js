cc07e0cdf56a346c1e37b7f1a3c6b79a00fdd0f0
ebialale <63908688+ebialale@users.noreply.github.com>
2020-04-23 20:44:30 +0100

    working version

---
+++ array.js
@@ -0,0 +1,17 @@
+var numbers = [3, 5, 6, 7, 3];
+
+
+// var middleItem = [(numbers.length - 1) / 2];
+// console.log(middleItem)
+
+function calculate(array) {
+    // first item + last item - (last item + 1) / 2
+//first item + last item (array.length-1) + middle item (array.length)
+    //var middleIndex = (array.length - 1);
+
+    var ans = array[0] + array[array.length - 1] - array[(array.length - 1) / 2];
+    return ans;
+}
+
+
+console.log(calculate(numbers))
