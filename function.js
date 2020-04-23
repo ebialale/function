d48b9fbbae680097ea190aa425a420ecaa619929
ebialale <63908688+ebialale@users.noreply.github.com>
2020-04-23 12:34:26 +0100

    final

--- function.js
+++ function.js
@@ -1,36 +1,36 @@
 
-// console.log(numTest(9))
+console.log(numTest(9))
 
-// function numTest(number) {
-// 	if (number % 2 === 0) {
-// 		return "bar";
-// 	} else if (isPrimeNumber(number) === true) {
-// 	return "foobar";
-// 	} else if ( number % 2 === 1) {
-// 		return "foo";
-// 	} else {
-// 		return "Something went wrong!";		
-// 	}
-// }
-
-
+function numTest(number) {
+	if (number % 2 === 0) {
+		return "bar";
+	} else if (isPrimeNumber(number) === true) {
+	return "foobar";
+	} else if ( number % 2 === 1) {
+		return "foo";
+	} else {
+		return "Something went wrong!";		
+	}
+}
 
 
 
-// function isPrimeNumber(number) {
-// 	let numberPrime = true;
 
-// 	for (var i = 2; i < number; i++) {
-// 		// This condition checks if the number passed can be divided by any number other than itself.
-// 		// If it finds a number that divides it, then it is not a prime number  		
-// 		if ( number % i === 0) {
-// 			numberPrime = false;
-// 			break;
-// 		}
-// 	}
 
-// 	return numberPrime;
-// }
+function isPrimeNumber(number) {
+	let numberPrime = true;
+
+	for (var i = 2; i < number; i++) {
+		// This condition checks if the number passed can be divided by any number other than itself.
+		// If it finds a number that divides it, then it is not a prime number  		
+		if ( number % i === 0) {
+			numberPrime = false;
+			break;
+		}
+	}
+
+	return numberPrime;
+}
 
 
 
@@ -50,25 +50,6 @@
     
 // }
 
-// console.log(multiply(3, 6))
-// function multiply(num1, num2) {
-// 	var ans = num1 * num2;
-// 	return ans;
-// }
 
 
 
-var numbers = [3, 5, 6, 7, 3, 4, 9, 3, 1];
-
-// var middleItem = [(numbers.length - 1) / 2];
-// console.log(middleItem)
-
-function calculate(array) {
-	// first item + last item - (last item + 1) / 2
-//first item + last item (array.length-1) + middle item (array.length)
-	//var middleIndex = (array.length - 1);
-	var ans = array[0] + array[array.length - 1] - array[(array.length - 1) / 2];
-	return ans;
-}
-
-console.log(numbers.length)
--- test.js
+++
@@ -1,8 +0,0 @@
-const x = 4;
-const num = [];
-
-for (var i = 0; i < x; i++); {
-    num.push(i + 1)
-}
-
-console.log(num);
