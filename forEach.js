a169b70155aaaa91388fd3eac149e049a2e9e3e8
ebialale <63908688+ebialale@users.noreply.github.com>
2020-04-24 10:33:19 +0100

    for each

---
+++ forEach.js
@@ -0,0 +1,9 @@
+const colors = [1, 12, 4];
+var sum = 0;
+
+colors.forEach(function myFinction(num) {
+    sum += num;
+})
+
+console.log(sum)
+
---
+++ forloop.js
@@ -0,0 +1,20 @@
+const numbers = [3, 5, 6, 7, 3, 4, 7];
+
+
+var max = numbers.length;
+//var sum = numbers[i];
+
+var ans = numbers[0] + numbers[numbers.length - 1]
+
+for (var i = 0; i < max; i++) {
+    
+   console.log(numbers[i]);
+}
+
+
+
+
+
+numbers.forEach(function(num) {
+    console.log(num)
+})
