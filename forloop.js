4d66ae117390d13fed15b77d4502df25171c598b
ebialale <63908688+ebialale@users.noreply.github.com>
2020-07-06 02:03:09 +0100

    Latest

--- forloop.js
+++ forloop.js
@@ -1,11 +1,10 @@
 const numbers = [3, 5, 6, 7, 3, 4, 7];
 
-var max = numbers.length;
+var total = numbers.length;
 var sum = 0;
 
-for (var i = 0; i < max; i++) {
+for (var i = 0; i < total; i++) {
     sum += numbers[i]; 
 }
 
 console.log(sum)
-
